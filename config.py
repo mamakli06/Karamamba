f16e6bb6ac743e999f95fc0b889ec2158d60b3da
mamakli06 <83599873+mamakli06@users.noreply.github.com>
2025-05-09 21:54:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -59,6 +59,8 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 314572800))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 3221225472))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", False)
+
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
